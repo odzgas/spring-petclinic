e93c8c9a88b2e02068146e1ae2bacb1ab91b98eb
Odzga <slawomir.odzga@capgemini.com>
2021-11-21 22:24:38 +0000

    znów mvnw clean package

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,7 +16,7 @@ pipeline {
 
         stage('Build') {
             steps {
-                sh './mvnw clean compile'
+                sh './mvnw clean package'
             }
 
             post {
