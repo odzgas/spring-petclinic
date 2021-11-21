36e961c62b5b8dbc06dcf0a7de7372fa2b40d27a
Odzga <slawomir.odzga@capgemini.com>
2021-11-21 22:12:42 +0000

    poprawione w wysyłaniu maila recipentProviders na recipientProviders

--- pipeline.groovy
+++ pipeline.groovy
@@ -25,6 +25,14 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+//                }
+//                changed {
+                    emailext subject: 'Job \'${JOB_NAME}\'(${BUILD_NUMBER}) is waiting for input\n',
+                    body: 'Please go to $(BUILD_URL} and verify the build',
+                    attachLog: true,
+                    compressLog: true,
+                    to: 'test@jenkins',
+                    recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
