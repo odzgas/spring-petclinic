08d860453c0c37b77c88bfebef532bf7569bf0d0
Odzga <slawomir.odzga@capgemini.com>
2021-11-22 14:13:28 +0000

    zakomentowana część post actions wraz z testami, a testy sie odpalaja...

--- pipeline.groovy
+++ pipeline.groovy
@@ -19,22 +19,26 @@ pipeline {
                 sh './mvnw clean package'
             }
 
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-               // }
-               // changed {
-                    emailext subject: 'Job \'${JOB_NAME}\'(${BUILD_NUMBER}) is waiting for input\n',
+//            post {
+//                // If Maven was able to run the tests, even if some of the test
+//                // failed, record the test results and archive the jar file.
+//                always {
+//                    junit '**/target/surefire-reports/TEST-*.xml'
+//                    archiveArtifacts 'target/*.jar'
+//                }
+//
+//            }
+        }
+        stage('Email') {
+            steps {
+                emailext subject: 'Job \'${JOB_NAME}\'(${BUILD_NUMBER}) is waiting for input\n',
                         body: 'Please go to $(BUILD_URL} and verify the build',
                         attachLog: true,
                         compressLog: true,
                         to: 'test@jenkins',
                         recipientProviders: [upstreamDevelopers(), requestor()]
-                }
             }
         }
+
     }
 }
